b30b1f43969767d6c38fc63aa6522aca71859ed8
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-04-13 13:51:57 +0500

    Update and rename cpmsophia.py to yameencpm.py

--- yameencpm.py
+++ yameencpm.py
@@ -1,8 +1,8 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://cpmsophia.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://yameencpm.squareweb.app/api"
 
-class CPMSophia:
+class YameenCPM:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
