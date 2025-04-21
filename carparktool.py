e5956d9ad39fa1e5983898072b300a7d01e73e06
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-04-21 17:05:04 +0500

    Update carparktool.py

--- carparktool.py
+++ carparktool.py
@@ -1,7 +1,7 @@
 import requests
 from time import sleep
 
-BASE_URL: str = "https://darkorange-mosquito-826916.hostingersite.com/api"
+BASE_URL: str = "https://dodgerblue-scorpion-385977.hostingersite.com/api"
 
 class CarParkTool:
     def __init__(self, access_key) -> None:
